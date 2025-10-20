f8e642e70f3c8e0a2af63234299d9fd8ea6aed23
FiqriSharel <fiqrisharel56@gmail.com>
2025-10-20 21:09:17 +0800

    updated

--- Easy/gcd.py
+++ Easy/gcd.py
@@ -5,20 +5,21 @@ def gcd(m,n):
     primeM = list()
     primeN = list()
     for i in primeNum:
-        while True:
-            if m % i == 0:
-                primeM.append(i)
-                m = m / i
-            else:
-                break
+        if m > i or n > i:
+            while True:
+                if m % i == 0:
+                    primeM.append(i)
+                    m = m / i
+                else:
+                    break
 
-            if n % i == 0:
-                primeN.append(i)
-                n = n / i
-            else:
-                break
+                if n % i == 0:
+                    primeN.append(i)
+                    n = n / i
+                else:
+                    break
     CPF = [x for x in primeM if x in primeN]
     GCD = math.prod(CPF)
     return GCD
 
-print(gcd(60, 24))  
+print(gcd(60, 24))  # test case
