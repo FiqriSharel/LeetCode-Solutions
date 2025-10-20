2abbfb218b1af5be64cf0e1f57b558aef599581a
FiqriSharel <fiqrisharel56@gmail.com>
2025-10-20 20:53:24 +0800

    gcd

---
+++ Easy/gcd.py
@@ -0,0 +1,24 @@
+import math
+primeNum = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47}
+
+def gcd(m,n): 
+    primeM = list()
+    primeN = list()
+    for i in primeNum:
+        while True:
+            if m % i == 0:
+                primeM.append(i)
+                m = m / i
+            else:
+                break
+
+            if n % i == 0:
+                primeN.append(i)
+                n = n / i
+            else:
+                break
+    CPF = [x for x in primeM if x in primeN]
+    GCD = math.prod(CPF)
+    return GCD
+
+print(gcd(60, 24))  
